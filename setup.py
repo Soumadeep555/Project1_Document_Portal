c0c2e752c4f239bc1227cd7ab75279eaa95d7e95
Soumadeep Das <soumadeep154@gmail.com>
2025-07-23 14:10:25 +0100

    Folder Structure Updated with init and setup.py also updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PROJECT1_DOCUMENT_PORTAL",
+    author="Soumadeep Das",
+    description="Document Analysis, Chatbot and Comparision Portal with Advanced RAG",
+    version="0.1",
+    packages=find_packages(),
+)
