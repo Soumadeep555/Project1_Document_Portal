c2bbd947d88efb01ae0772a07ae22aeee6c0cc71
Soumadeep Das <soumadeep154@gmail.com>
2025-08-02 12:09:14 +0100

    testing completed for document comparator

--- src/document_compare/document_comparator.py
+++ src/document_compare/document_comparator.py
@@ -4,7 +4,7 @@ import pandas as pd
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import *
-from prompt.prompt_library import PROPMT_REGISTRY
+from prompt.prompt_library import PROMPT_REGISTRY
 from utils.model_loader import ModelLoader
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
@@ -17,8 +17,8 @@ class DocumentComparatorLLM:
         self.llm = self.loader.load_llm()
         self.parser = JsonOutputParser(pydantic_object=SummaryResponse)
         self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
-        self.prompt = PROPMT_REGISTRY["document_comparison"]
-        self.chain = self.prompt | self.llm | self.parser | self.fixing_parser
+        self.prompt = PROMPT_REGISTRY["document_comparison"]
+        self.chain = self.prompt | self.llm | self.parser  
         self.log.info("DocumentComparatorLLM initialized with model and parser.")
 
     def compare_documents(self,combined_docs: str) -> pd.DataFrame:
