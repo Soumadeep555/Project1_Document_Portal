8b081d9859037a7ea78193c869334348874bdfdc
Soumadeep Das <soumadeep154@gmail.com>
2025-08-28 14:51:21 +0100

    issue fix 2

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from utils.config_loader import load_config
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
-from logger.custom_logger import GLOBAL_LOGGER as log
+from logger import GLOBAL_LOGGER as log
 from exception.custom_exception import DocumentPortalException
 
 
